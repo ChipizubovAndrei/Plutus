1057a4b0bc2f8d74d629345fbec0cd6e2a142a00
Chipizubov Andrei <chipizubow.andrej@gmail.com>
2024-10-23 18:51:05 +0700

    Добавлены тесты для класса User. В модуле core файлы разбиты на include и src.

--- test/core/UserTest.cpp
+++ test/core/UserTest.cpp
@@ -9,7 +9,15 @@ UserTest::UserTest(QObject *parent)
 {}
 
 void UserTest::initTest()
-{}
+{
+	User user("Andrei");
+	QCOMPARE(QString("Andrei"), user.getName());
+}
+
+void UserTest::testForTest()
+{
+	QCOMPARE(1, 1);
+}
 
 QTEST_MAIN(UserTest)
 
--- test/core/UserTest.h
+++ test/core/UserTest.h
@@ -11,4 +11,5 @@ public:
 
 private slots:
 	void initTest();
+	void testForTest();
 };
