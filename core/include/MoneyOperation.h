7dd226da3efa4cc36fa3b09e28fbe6151e8484e0
Chipizubov Andrei <chipizubow.andrej@gmail.com>
2024-10-31 18:16:51 +0700

    Remove comments from MoneyOperation.

--- core/include/MoneyOperation.h
+++ core/include/MoneyOperation.h
@@ -3,14 +3,11 @@
 #include <QDate>
 #include <QString>
 
-/*
-Структура реализует одну операцию по счету пользователя
-*/
 struct MoneyOperation
 {
-	QDate date; // Дата совершения операции
-	bool isInCome; // Вид операции (доход/расход)
-	QString category; // Категория (аренда, з/п и т.д.)
+	QDate date;
+	bool isInCome;
+	QString category;
 	int moneyAmount;
-	QString note; // Комментарии пользователя
+	QString note;
 };
