c65f52a91d402b68ddd8e496a4ac7fddecc1e255
Chipizubov Andrei <chipizubow.andrej@gmail.com>
2024-10-31 18:17:45 +0700

    Test updates.

---
+++ test/core/MoneyOperationTest.cpp
@@ -0,0 +1,3 @@
+#include <MoneyOperation.h>
+
+
--- test/core/UserTest.cpp
+++ test/core/UserTest.cpp
@@ -1,26 +1,9 @@
-#include "UserTest.h"
-
-#include <QTest>
-#include <QSharedPointer>
+#include <gtest/gtest.h>
 
 #include <User.h>
 
-UserTest::UserTest(QObject *parent)
-	: QObject(parent)
-{}
-
-void UserTest::initTest()
+TEST(UserClassTest, initialTest)
 {
 	User user("Andrei");
-	QCOMPARE(QString("Andrei"), user.getName());
+	EXPECT_EQ(user.getName(), "Andrei");
 }
-
-void UserTest::copyConstractorTest()
-{
-	QSharedPointer<User> userOriginal = QSharedPointer<User>(new User ("Andrei"));
-	QSharedPointer<User> userCopy(userOriginal);
-	QCOMPARE(userOriginal.data(), userCopy.data());
-}
-
-QTEST_MAIN(UserTest)
-
