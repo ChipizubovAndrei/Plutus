e490adcb57ea542cc3f66b25f99a6a5a453c8da9
Chipizubov Andrei <chipizubow.andrej@gmail.com>
2024-12-08 09:43:27 +0700

    added model for operation table

---
+++ modules/MoneyOperationBrowser/MoneyOperationTableModel.cpp
@@ -0,0 +1,120 @@
+﻿#include "MoneyOperationTableModel.h"
+
+#include <QDateTime>
+#include <QSqlQuery>
+#include <QSqlError>
+
+#include <DatabaseManager.h>
+
+MoneyOperationTableModel::MoneyOperationTableModel()
+    : mTableName(DatabaseManager::getOperationTableName())
+{
+    refresh();
+}
+
+QVariant MoneyOperationTableModel::data(const QModelIndex& index, int role) const
+{
+    QVariant value = QSqlQueryModel::data(index);
+
+    // Какие либо преобразования с данными
+    // Например преобразование значений к определенному виду
+    if (index.column() == 0) return value.toInt(); // ID операции
+    if (index.column() == 1) return value.toInt(); // source account id
+    if (index.column() == 2) return value.toInt(); // destination account id
+    if (index.column() == 3) return value.toInt(); // memder id
+    if (index.column() == 4) return value.toInt(); // category id
+    if (index.column() == 5)
+    {
+        // Дата совершения операции, храниться как юлианская дата
+        return QDate::fromJulianDay(value.toInt()).toString("YYYY.MM.DD");
+    }
+    if (index.column() == 6)
+    {
+        // Сумма денег в операции
+        return QString("%1").arg(value.toDouble(), 0, 'f', 2);
+    }
+    if (index.column() == 7) value.toString(); // note
+
+    return value;
+}
+
+bool MoneyOperationTableModel::setData(const QModelIndex& index, const QVariant& value, int role)
+{
+    int column = 0;
+    if (column < 1) return false;
+    QModelIndex primaryKeyIndex = QSqlQueryModel::index(index.row(), 0);
+    int id = QSqlQueryModel::data(primaryKeyIndex).toInt();
+
+    QSqlQuery query;
+    if (column == 1)
+    {
+        query.prepare("UPDATE ? SET srcAccount_id = ? WHERE id = ?");
+        query.addBindValue(mTableName);
+        query.addBindValue(value.toInt());
+        query.addBindValue(id);
+
+    }
+    else if (column == 2)
+    {
+        query.prepare("UPDATE ? SET dstAccount_id = ? WHERE id = ?");
+        query.addBindValue(mTableName);
+        query.addBindValue(value.toInt());
+        query.addBindValue(id);
+    }
+    else if (column == 3)
+    {
+        query.prepare("UPDATE ? SET member_id = ? WHERE id = ?");
+        query.addBindValue(mTableName);
+        query.addBindValue(value.toInt());
+        query.addBindValue(id);
+    }
+    else if (column == 4)
+    {
+        query.prepare("UPDATE ? SET category_id = ? WHERE id = ?");
+        query.addBindValue(mTableName);
+        query.addBindValue(value.toInt());
+        query.addBindValue(id);
+    }
+    else if (column == 5)
+    {
+        query.prepare("UPDATE ? SET date = ? WHERE id = ?");
+        query.addBindValue(mTableName);
+        query.addBindValue(value.toDate().toJulianDay());
+        query.addBindValue(id);
+    }
+    else if (column == 6)
+    {
+        query.prepare("UPDATE ? SET moneyAmount = ? WHERE id = ?");
+        query.addBindValue(mTableName);
+        query.addBindValue(value.toDouble());
+        query.addBindValue(id);
+    }
+    else if (column == 7)
+    {
+        query.prepare("UPDATE ? SET note = ? WHERE id = ?");
+        query.addBindValue(mTableName);
+        query.addBindValue(value.toString());
+        query.addBindValue(id);
+    }
+    bool ok = query.exec();
+    qDebug() << "Изменение данных в таблице Operations завершилось со статусом: "
+             << query.lastError().text();
+    refresh();
+    return ok;
+
+}
+
+void MoneyOperationTableModel::refresh()
+{
+    setQuery(QString("SELECT * FROM %1 ORDER BY id DESC").arg(mTableName));
+
+    setHeaderData(0, Qt::Horizontal, "id");
+    setHeaderData(1, Qt::Horizontal, "srcAccount_id");
+    setHeaderData(2, Qt::Horizontal, "dstAccount_id");
+    setHeaderData(3, Qt::Horizontal, "member_id");
+    setHeaderData(4, Qt::Horizontal, "category_id");
+    setHeaderData(5, Qt::Horizontal, "date");
+    setHeaderData(6, Qt::Horizontal, "moneyAmount");
+    setHeaderData(7, Qt::Horizontal, "note");
+
+}
---
+++ modules/MoneyOperationBrowser/MoneyOperationTableModel.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QSqlQueryModel>
+#include <QModelIndex>
+#include <QVariant>
+
+#include <MoneyOperation.h>
+
+class MoneyOperationTableModel : public QSqlQueryModel
+{
+	Q_OBJECT
+
+public:
+	MoneyOperationTableModel();
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    bool setData(const QModelIndex& index, const QVariant& value, int role=Qt::EditRole) override;
+
+private:
+    void refresh();
+
+private:
+    QString mTableName;
+};
