0724bf8a2dc58442e2863337dd33d44a78910888
Chipizubov Andrei <chipizubow.andrej@gmail.com>
2024-12-11 21:11:31 +0700

    added category manager

---
+++ database/CategoryManager.cpp
@@ -0,0 +1,37 @@
+﻿#include "CategoryManager.h"
+
+#include <QSqlQuery>
+#include <QSqlError>
+
+#include "DatabaseManager.h"
+
+static QSharedPointer<CategoryManager> categoryManager;
+
+QSharedPointer<CategoryManager> CategoryManager::instance()
+{
+    if (!categoryManager)
+    {
+        categoryManager = QSharedPointer<CategoryManager>(new CategoryManager());
+    }
+    return categoryManager;
+}
+
+CategoryManager::CategoryManager()
+    : mTableName(DatabaseManager::getCategoryTableName())
+{
+    qDebug() << "Начато создание менеджера категорий";
+    QSqlQuery query("SELECT * FROM ?");
+    query.addBindValue(mTableName);
+    query.exec();
+    qDebug() << "Получение данных завершилось со статусом " << query.lastError().text();
+    if (query.lastError().type() == QSqlError::NoError)
+    {
+        query.setForwardOnly(true);
+        while (query.next())
+        {
+            mCategories[query.value(0).toInt()] = query.value(1).toString();
+        }
+        qDebug() << QStringLiteral("Из базы данных получено %1 категорий").arg(mCategories.size());
+    }
+    qDebug() << "Окончено соуздание менеджера категорий";
+}
---
+++ database/CategoryManager.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QSharedPointer>
+#include <QString>
+#include <QMap>
+
+class CategoryManager : public QObject
+{
+	Q_OBJECT
+
+public:
+    static QSharedPointer<CategoryManager> instance();
+    ~CategoryManager() = default;
+
+signals:
+    void categoryAdded(const QString& category);
+    void categoryRemoved(const QString& category);
+
+public slots:
+
+private:
+	CategoryManager();
+    CategoryManager(const CategoryManager& other);
+    const CategoryManager& operator=(const CategoryManager& other);
+
+private:
+    QMap<int, QString> mCategories;
+    QString mTableName;
+};
