4425f6e2f3e3148fcd9019be9c89d985257182d4
Chipizubov Andrei <chipizubow.andrej@gmail.com>
2025-01-12 10:54:03 +0700

    feat(database): added operation manager

---
+++ database/OperationManager.cpp
@@ -0,0 +1,23 @@
+﻿#include "OperationManager.h"
+
+#include "DatabaseManager.h"
+
+static QSharedPointer<OperationManager> operationManager;
+
+QSharedPointer<OperationManager> OperationManager::instance()
+{
+    if (!operationManager)
+    {
+        operationManager = QSharedPointer<OperationManager>(
+            new OperationManager()
+        );
+    }
+    return operationManager;
+}
+
+OperationManager::OperationManager(QObject *parent)
+	: QObject(parent)
+{
+    mOperationTableName = DatabaseManager::getOperationTableName();
+
+}
---
+++ database/OperationManager.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QSharedPointer>
+
+#include <MoneyOperation.h>
+
+class OperationManager : public QObject
+{
+	Q_OBJECT
+
+public:
+    static QSharedPointer<OperationManager> instance();
+
+    void addOperation(const MoneyOperation& operation);
+    void removeOperation(const MoneyOperation& operation);
+    void updateOperation(const MoneyOperation& operation);
+
+signals:
+    void operationAdded(const MoneyOperation& operation);
+    void operationRemoved(const MoneyOperation& operation);
+    void operationUpdated(const MoneyOperation& operation);
+
+private:
+	OperationManager(QObject *parent=nullptr);
+    OperationManager(const OperationManager& other);
+    OperationManager& operator=(const OperationManager& other);
+
+private:
+    QString mOperationTableName;
+};
